da19b52531044c991d199cb84bd9073222df0b91
Tran Kevin <trankevinmay@gmail.com>
2022-03-18 11:50:54 +0100

    Test 2 terminer

--- 3.2.Test2/questions.js
+++ 3.2.Test2/questions.js
@@ -1,43 +1,60 @@
-let CreationTableauLangages =  () => {
-
+let CreationTableauLangages = () => {
+    let arr = ["Html", "CSS", "Java", "PHP"]
+    return arr
 }
 
-let CreationTableauNombres =  () => {
-
+let CreationTableauNombres = () => {
+    let arr = [0, 1, 2, 3, 4, 5]
+    return arr
 }
 
-let RemplacementElement =  (langages) => {
-
+let RemplacementElement = (langages) => {
+    langages[2] = "Javascript"
+    return langages
 }
 
-let AjoutElementLangages =  (langages) => {
-
+let AjoutElementLangages = (langages) => {
+    langages.push("Ruby", "Python")
+    return langages
 }
 
-let AjoutElementNombres =  (nombres) => {
-
+let AjoutElementNombres = (nombres) => {
+    nombres.unshift(-2, -1)
+    return nombres
 }
 
-let SuppressionPremierElement =  (langages) => {
-
+let SuppressionPremierElement = (langages) => {
+    langages.shift()
+    return langages
 }
 
-let SuppressionDernierElement =  (langages) => {
-
+let SuppressionDernierElement = (langages) => {
+    langages.pop()
+    return langages
 }
 
-let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
-
+let ConversionChaineTableau = (reseaux_sociaux_chaine) => {
+    let arr = reseaux_sociaux_chaine.split(",")
+    return arr
 }
 
-let ConversionTableauChaine =  (langages) => {
-
+let ConversionTableauChaine = (langages) => {
+    let string = ""
+    string = langages.join(",")
+    return string
 }
 
-let TriTableau =  (reseaux_sociaux) => {
-
+let TriTableau = (reseaux_sociaux) => {
+    return reseaux_sociaux.sort()
 }
 
-let InversionTableau =  (langages) =>{
-
+let InversionTableau = (langages) => {
+    let arr = []
+    let x = 0;
+    for(i=langages.length -1 ; i >=0; i--){
+        arr[x] = langages[i]
+        x++;
+    }
+    
+    return arr
 }
