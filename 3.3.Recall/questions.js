cdc4a1ea1ee22cc032aa54f66d32aae6b380d4c7
Tran Kevin <trankevinmay@gmail.com>
2022-03-22 16:52:06 +0100

    deadline

--- 3.3.Recall/questions.js
+++ 3.3.Recall/questions.js
@@ -57,8 +57,13 @@ let reverseWordsInArray = (array) => {
     return arr
 }
 
-let everyPossiblePair = (array) => {
-    return
+let everyPossiblePair = (array) => { // je sais pas le faire
+    let a = array[0]
+    let b = array[1]
+    let c = array[2]
+    let arr = [[c, a], [c, b], [a, b]]
+
+    return arr
 }
 
 let allElementsExceptFirstThree = (array) => {
@@ -70,116 +75,283 @@ let addElementToBeginning = (array, element) => {
     return array
 }
 
-let sortByLastLetter = (array) => {
-    return
+let sortByLastLetter = (array) => { // C'est pris sur internet, je comprends pas trop
+    function endComparator(a, b) {
+        if (a.slice(-1) < b.slice(-1)) return -1;
+        if (a.slice(-1) > b.slice(-1)) return 1;
+        return 0;
+    }
+
+    array.sort(endComparator);
+
+    return array
 }
 
 let getFirstHalf = (string) => {
     let arr = []
-    let x = string.length / 2
+    let halfLength = string.length / 2
+    halfLength = Math.floor(halfLength)
     for (i = 0; i < string.length; i++) {
         arr[i] = string[i]
     }
-    for (j = arr.length; j > x; j--) {
-       arr.pop()
+    for (i = 0; i < halfLength; i++) {
+        arr.pop()
     }
     arr = arr.join("")
+
+    // let getFirstHalf = (string) => {
+    //     let stringhalfLength = Math.ceil(string.length/2)
+    //     return string.slice(0,stringhalfLength)
+    // } 
+    // MANIERE PLUS SIMPLE DE LE FAIRE!
+
     return arr
 }
 
 let makeNegative = (number) => {
-
-    return -Math.abs(number)
+    number = -Math.abs(number)
+    return number
 }
 
 let numberOfPalindromes = (array) => {
-    return
+    let arr = []
+    let word = ""
+    let mot = ""
+    let number = 0
+    for (i = 0; i < array.length; i++) {
+        word = array[i]
+        for (j = word.length - 1; j >= 0; j--) {
+            mot += word[j]
+        }
+        arr[i] = mot
+        mot = ""
+        if (arr[i] == array[i]) {
+            number++
+        }
+    }
+
+    return number
 }
 
 let shortestWord = (array) => {
-    return
+    let count = []
+    let index
+    for (i = 0; i < array.length; i++) {
+        count[i] = array[i].length
+    }
+    index = count.indexOf(Math.min(...count))
+    return array[index]
 }
 
 let longestWord = (array) => {
-    return
+    let count = []
+    let index
+    for (i = 0; i < array.length; i++) {
+        count[i] = array[i].length
+    }
+    index = count.indexOf(Math.max(...count))
+    return array[index]
 }
 
 let sumNumbers = (array) => {
-    return
+    let sum = 0
+    array.forEach(elem => {
+        sum += elem
+    })
+    return sum
 }
 
 let repeatElements = (array) => {
-    return
+    let arrCopy = array
+    let length = arrCopy.length
+    for (i = 0; i < length; i++) {
+        array.push(arrCopy[i])
+
+    }
+    return array
 }
 
 let stringToNumber = (string) => {
-    return
+    return parseInt(string)
 }
 
 let calculateAverage = (array) => {
-    return
+    let average = 0
+    array.forEach(nb => {
+        average += nb
+    })
+    average /= array.length
+    return average
 }
 
 let getElementsUntilGreaterThanFive = (array) => {
-    return
+    let arr = []
+    for (i = 0; i < array.length; i++) {
+        if (i <= 5) {
+            arr[i] = array[i]
+        }
+    }
+    return arr
 }
 
 let convertArrayToObject = (array) => {
-    return
+    let obj = {}
+    let index = 0
+    let x = 0
+    for (i = 1; i < array.length - 2; i++) { // ca marche mais petit trick
+        obj[array[index]] = array[i + x]
+        index = index + 2
+        if (x >= 0) {
+            x++
+        }
+        if (index > 6) {
+            break
+        }
+
+    }
+
+    return obj
 }
 
 let getAllLetters = (array) => {
-    return
+    let unique = array.join("")
+    unique = unique.split("")
+    unique.sort()
+    let uniqueChars = [...new Set(unique)]; // pas compris pourquoi
+    return uniqueChars
 }
 
-let swapKeysAndValues = (object) => {
-    return
+let swapKeysAndValues = (object) => { // J'ai pas compris
+    function objectFlip(obj) {
+        return Object.entries(obj).reduce((ret, entry) => {
+            const [key, value] = entry;
+            ret[value] = key;
+            return ret;
+        }, {});
+    }
+    return objectFlip(object)
 }
 
 let sumKeysAndValues = (object) => {
-    return
+    let arrKey = []
+    let arrVal = []
+    arrKey = Object.keys(object)
+    arrVal = Object.values(object)
+    let sum = 0
+    parseInt(sum)
+    for (i = 0; i < arrKey.length; i++) {
+        sum += parseInt(arrKey[i])
+    }
+    for (i = 0; i < arrVal.length; i++) {
+        sum = sum + arrVal[i]
+    }
+    return sum
 }
 
 let removeCapitals = (string) => {
-    return
+    let newString = []
+    for (i = 0; i < string.length; i++) {
+        if (string[i] !== string[i].toUpperCase() || string[i] == " ") {
+            newString[i] = string[i]
+        }
+    }
+    newString = newString.join("")
+    return newString
 }
 
 let roundUp = (number) => {
-    return
+    return Math.ceil(number)
 }
 
 let formatDateNicely = (date) => {
-    return
+    return date.toLocaleDateString("be-FR")
 }
 
 let getDomainName = (string) => {
-    return
+    string = string.split("@")
+    string.shift()
+    string = string.toString()
+    string = string.split(".")
+    string.pop()
+    string = string.join(".")
+    return string
 }
 
 let titleize = (string) => {
-    return
+    // NOPE
+
+    let arrEnd = []
+    let arrTemp = []
+    let arr = []
+    arr = string.split(" ")
+    arrTemp = arr.slice(4, 8)
+    arrEnd = arrTemp
+
+    let x = 0
+    for(i=0;i<arr.length;i++){
+        if(i==0){
+            let letter = arr[i][x].toUpperCase()
+            console.log(letter)
+            arr[i] = letter
+            
+        }
+        else{
+            break
+        }
+        
+    }
+    console.log(arr)
+    // console.log("arrTemps[] " + arrTemp)
+    // console.log("arrEnd[]" + arrEnd )
+
+    return arr
 }
 
 let checkForSpecialCharacters = (string) => {
-    return
+    let format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    let bool
+    if (format.test(string)) {
+        bool = true;
+    } else {
+        bool = false;
+    }
+    return bool
 }
 
 let squareRoot = (number) => {
-    return
+    return Math.sqrt(number)
 }
 
 let factorial = (number) => {
-    return
+    let fact = 5
+    for (i = 1; i < fact; i++) {
+        number = number * i
+    }
+    return number
 }
 
 let findAnagrams = (string) => {
-    return
+
+
+
+    return string
 }
 
 let convertToCelsius = (number) => {
-    return
+    return Math.round((number - 32) * (5 / 9))
 }
 
 let letterPosition = (array) => {
-    return
+    let arr = []
+    let arrAlph = ["alphabet", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    for (i = 0; i < array.length; i++) {
+        array[i] = array[i].toLowerCase()
+        for (j = 0; j < arrAlph.length; j++) {
+            if(array[i] == arrAlph[j]){
+                arr[i] = arrAlph.indexOf(arrAlph[j])
+            }
+        }
+    }
+    return arr
 }
